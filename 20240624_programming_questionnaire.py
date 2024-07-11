53378257bb874445b191faf1d3de74c82c1fe90e
Dan218347 <daniel.carrasquillo31@guttmanmail.cuny.edu>
2024-07-11 11:27:06 -0400

    Create 20240624_programming_questionnaire.py

---
+++ 20240624_programming_questionnaire.py
@@ -0,0 +1,11 @@
+def main():
+    name = "Daniel c"
+    print("we want to know if you like programming!")
+    print()
+    print("do you like programming " + name + "?")
+    answer = input()
+    print("great you said " + answer + "?")
+    print("let's start learning some python today!")
+
+if __name__ == "__main__":
+    main()
